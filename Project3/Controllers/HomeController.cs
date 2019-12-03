7615b3a3ef5f710a678a73a4ba6d87667efdc65e
Soi con <soicon78946@gmail.com>
2019-12-03 13:02:06 +0700

    init project

---
+++ Project3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET:
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult FriendList()
+        {
+            return View();
+
+        }
+        public ActionResult MessagePage()
+        {
+            return View();
+        }
+        
+
+    }
+}
